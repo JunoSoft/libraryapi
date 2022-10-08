a62beb187934c091c4acf06b3d63da681a6db96a
Junosoft <junosoftltd@gmail.com>
2022-10-08 22:39:26 +0200

    database connect

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const book = require("./routers/createBook")
 const app = express();
 
+mongoose.connect("mongodb://localhost/libarydb")
+.then(()=>console.log("DATABASE Conneted!"));
 
-app.use(express.json()) 
+
+app.use(express.json()) ;
 app.use("/api/",book);
 
-app.listen(3000,()=>console.log("SERVER IS well Connected--PORT: 3000"))
+app.listen(3000,()=>console.log("SERVER IS well Connected--PORT: 3000"));
