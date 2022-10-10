d84267c34b387e99cff8946c65038935538409b1
Junosoft <junosoftltd@gmail.com>
2022-10-10 20:42:32 +0200

    edited users controller

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,17 @@ const User = require('../models/user')
 const createUser = async (req,res)=>{
 function validation(user){
 const schema = {
-  name:Joi.string().required().min(3)
+  name:Joi.string().required().min(3),
+  age:Joi.number(),
+  interested:Joi.string(),
 }
 return Joi.validate(user,schema)
 }
 
 let newUser = new User({
-  name: req.body.name
+  name: req.body.name,
+  age:req.body.age,
+  interested:req.body.interestedt
 })
 const {error} = validation(req.body);
 if(error) res.status(400).send(error.details[0].message)
