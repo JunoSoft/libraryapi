fb823322916844d36962049f627486031fcaaf0f
Junosoft <junosoftltd@gmail.com>
2022-10-16 23:06:52 +0200

    added files

--- routers/orders.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const {createUser} = require("../controllers/users")
-const router = express.Router();
-
-router.post("/createuser",createUser)
-
-module.exports = router
